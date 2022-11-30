f0db6f38e5b85290a9a541b9baadd7a768686f44
Jonathan <castillojonathan068@gmail.com>
2022-11-30 10:55:20 -0600

    Initial Commit

---
+++ JonathanEdgarAlberto_finalProject/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  JonathanEdgarAlberto_finalProject
+//
+//  Created by Alumno on 30/11/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ JonathanEdgarAlberto_finalProject/JonathanEdgarAlberto_finalProjectApp.swift
@@ -0,0 +1,17 @@
+//
+//  JonathanEdgarAlberto_finalProjectApp.swift
+//  JonathanEdgarAlberto_finalProject
+//
+//  Created by Alumno on 30/11/22.
+//
+
+import SwiftUI
+
+@main
+struct JonathanEdgarAlberto_finalProjectApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
