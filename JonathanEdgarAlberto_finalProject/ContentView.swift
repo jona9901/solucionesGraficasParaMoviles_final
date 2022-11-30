04679a6a5161859ac845844c6a3ee330d92d6f8d
Jonathan <castillojonathan068@gmail.com>
2022-11-30 12:48:33 -0600

    Login ui

--- JonathanEdgarAlberto_finalProject/ContentView.swift
+++ JonathanEdgarAlberto_finalProject/ContentView.swift
@@ -2,20 +2,72 @@
 //  ContentView.swift
 //  JonathanEdgarAlberto_finalProject
 //
-//  Created by Alumno on 30/11/22.
+//  Created by Jonathan Castillo on 30/11/22.
 //
 
 import SwiftUI
 
+let lightGreyColor = Color(red: 239.0/255.0, green: 243.0/255.0, blue: 244.0/255.0, opacity: 1.0)
+
 struct ContentView: View {
+    
+    @State var username: String = ""
+    @State var password: String = ""
+    
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        VStack {
+            welcomeText()
+            userImage()
+            TextField("Username", text: $username)
+                .padding()
+                .background(lightGreyColor)
+                .cornerRadius(5.0)
+                .padding(.bottom, 20)
+            SecureField("Password", text: $password)
+                .padding()
+                .background(lightGreyColor)
+                .cornerRadius(5.0)
+                .padding(.bottom, 20)
+            Button(action: {print("Button tapped")}, label: {
+                Text("LOGIN")
+                    .font(.headline)
+                    .foregroundColor(.white)
+                    .padding()
+                    .frame(width: 220, height: 60)
+                    .background(Color.blue)
+                    .cornerRadius(15.0)
+            })
+            Button("Sign in") {
+                
+            }
+        }
+        .padding()
     }
 }
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
         ContentView()
+    }
+}
+
+struct welcomeText: View {
+    var body: some View {
+        Text("Log in")
+            .font(.largeTitle)
+            .fontWeight(.semibold)
+            .padding(.bottom, 20)
+    }
+}
+
+struct userImage: View {
+    var body: some View {
+        Image("userImage")
+            .resizable()
+            .aspectRatio(contentMode: .fill)
+            .frame(width: 150, height: 150)
+            .clipped()
+            .cornerRadius(150)
+            .padding(.bottom, 75)
     }
 }
